04d4dc6b547abea01843f0be8f35ff54bb682ff6
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-02-24 03:40:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,9 @@
 #write your code here
 
-def countdown
-  n = 10
-  while t >= 0
+def countdown(n)
+  while n >= 0
     puts "#{n} SECONDS(S)!"
-    t -= 1
+    n -= 1
   end
   puts "HAPPY NEW YEAR!"
 end
